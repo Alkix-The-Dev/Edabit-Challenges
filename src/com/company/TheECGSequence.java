d86980741a07fa4a226bad6fc5ad96407f04267c
Alkix-The-Dev <artur.wlodarczyk12@gmail.com>
2022-01-01 18:48:10 +0100

    another stupid commit

--- src/com/company/TheECGSequence.java
+++ src/com/company/TheECGSequence.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class TheECGSequence {
     public static void main(String[] args) {
-        System.out.println("Hello ");
+        System.out.println("Hello  ");
     }
 }
