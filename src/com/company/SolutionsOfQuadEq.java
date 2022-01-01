a0b61fc513a64017eda08ec6c4354a78d0a93658
Artur Wlodarczyk <artur.wlodarczyk12@gmail.com>
2022-01-01 14:57:24 +0100

    Finished challenge with ammount of solutions of quadratic equation

---
+++ src/com/company/SolutionsOfQuadEq.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class SolutionsOfQuadEq {
+    public static int solutions(int a, int b, int c) {
+        if(a == 0) {
+            return 1;
+        }
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
