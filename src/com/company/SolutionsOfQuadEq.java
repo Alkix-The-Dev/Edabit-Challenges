73ed936a087d9e5df790fb6029825e56a512b27f
Artur Wlodarczyk <artur.wlodarczyk12@gmail.com>
2022-01-01 14:59:50 +0100

    remaking previous commit

--- src/com/company/SolutionsOfQuadEq.java
+++ src/com/company/SolutionsOfQuadEq.java
@@ -2,14 +2,21 @@ package com.company;
 
 public class SolutionsOfQuadEq {
     public static int solutions(int a, int b, int c) {
-        if(a == 0) {
-            return 1;
+        int delta = 0;
+
+        delta = b*b - 4 * a * c;
+
+        if(delta > 0) {
+            return 2;
         }
-        
-        return 0;
+        else if ( delta == 0 ) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
-
+        System.out.println(solutions(-1,0,1));
     }
 }
